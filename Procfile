7952beadee28f88a64cd9f225c9d7506a163ff81
abisubramanya27 <abisubramanya27@gmail.com>
2021-02-12 01:39:37 +0530

    Website done

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app.application:app
+web: gunicorn project:app
---
+++ project/class_orm.py
@@ -0,0 +1,13 @@
+from . import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer,primary_key = True)
+    shaastraID = db.Column(db.String(25),nullable = False,unique = True)
+    password = db.Column(db.String(100),nullable = False)
+    name = db.Column(db.String(50),nullable = False)
+    email = db.Column(db.String(50),nullable = False,unique=True)
+
+    def __init__(self, **kwargs) :
+        super(User, self).__init__(**kwargs)
